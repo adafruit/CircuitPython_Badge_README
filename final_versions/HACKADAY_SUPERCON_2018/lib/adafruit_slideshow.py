05d6c5a34ace12598097a823a5dbab7a18c56f86
Dylan Herrada <33632497+dherrada@users.noreply.github.com>
2021-01-29 15:12:03 -0500

    Moved adafruit_slideshow.py from pulseio.PWMOut to pwmio.PWMOut

--- final_versions/HACKADAY_SUPERCON_2018/lib/adafruit_slideshow.py
+++ final_versions/HACKADAY_SUPERCON_2018/lib/adafruit_slideshow.py
@@ -109,7 +109,7 @@ class SlideShow:
         import board
         import pulseio
 
-        slideshow = SlideShow(board.DISPLAY, pulseio.PWMOut(board.TFT_BACKLIGHT), folder="/",
+        slideshow = SlideShow(board.DISPLAY, pwmio.PWMOut(board.TFT_BACKLIGHT), folder="/",
                               loop=False, order=PlayBackOrder.ALPHABETICAL)
 
         while slideshow.update():
@@ -132,7 +132,7 @@ class SlideShow:
         brightness_up = touchio.TouchIn(board.TOUCH3)
         brightness_down = touchio.TouchIn(board.TOUCH2)
 
-        slideshow = SlideShow(board.DISPLAY, pulseio.PWMOut(board.TFT_BACKLIGHT), folder="/",
+        slideshow = SlideShow(board.DISPLAY, pwmio.PWMOut(board.TFT_BACKLIGHT), folder="/",
                               auto_advance=False, dwell=0)
 
         while True:
